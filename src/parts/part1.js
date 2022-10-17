0855f4101f3ad447c6e32375155c9e2188c6ca4b
Eli Frungorts <eli.frungorts@gmail.com>
2022-10-17 12:26:41 -0400

    added link to presentation

--- src/parts/part1.js
+++ src/parts/part1.js
@@ -1,7 +1,10 @@
 import React from "react";
-
 const Part1 = () => {
-    return <>P1</>;
+
+
+  return (
+    <a href="https://docs.google.com/presentation/d/1G1pfntMHyLOIBAuEqDBHK6iVaotP2WomTv2Kg9hzcR0/edit?usp=sharing" target='_blank'>Presentation</a>
+  )
 };
 
 export default Part1;
