d772f24b57352ea968c4f870abbc5e950e22f939
Eli Frungorts <eli.frungorts@gmail.com>
2022-11-09 12:17:06 -0500

    added p2

--- src/parts/part1.js
+++ src/parts/part1.js
@@ -1,10 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+`;
+
 const Part1 = () => {
-
-
   return (
-    <a href="https://docs.google.com/presentation/d/1G1pfntMHyLOIBAuEqDBHK6iVaotP2WomTv2Kg9hzcR0/edit?usp=sharing" target='_blank'>Presentation</a>
-  )
+    <Main>
+      <a
+        href="https://docs.google.com/presentation/d/1G1pfntMHyLOIBAuEqDBHK6iVaotP2WomTv2Kg9hzcR0/edit?usp=sharing"
+        target="_blank"
+      >
+        Presentation
+      </a>
+      <a
+        href="https://docs.google.com/document/d/1d1anVYKeOINgyLGJZHZS95lo-MnRCUp-CaIF_7vdupo/edit?usp=sharing"
+        target="_blank"
+      >
+        Documentation
+      </a>
+    </Main>
+  );
 };
 
 export default Part1;
--- src/parts/part2.js
+++ src/parts/part2.js
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+`;
 
 const Part2 = () => {
-    return <>P2</>;
+  return (
+    <Main>
+      <a
+        href="https://docs.google.com/presentation/d/1-pnpSfWn7Rr0_CLCYe9Wn-oHuzC4P_u2CQit7NLtB7Q/edit?usp=sharing"
+        target="_blank"
+      >
+        Presentation
+      </a>
+    </Main>
+  );
 };
 
 export default Part2;
